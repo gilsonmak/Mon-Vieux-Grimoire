7d39ac77dd2829c37adf282565ea784dab1d34ee
gilsonmak <gilsonmakanounou@yahoo.com>
2025-01-30 20:46:12 +0100

    feat: Ajout des routes User avec mongoose

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require ('mongoose');
 
+const userRoutes = require('./routes/user');
+
 const app = express();
 
 app.use(express.json());
@@ -22,5 +24,7 @@ app.use((req, res, next) => {
     next();
   });
 
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.post('/signup');
+router.post('/login');
+
+
+
+
+
+module.exports = router;
