44eaab4093079fce7071ca89f7eeed5aaba1123f
gilsonmak <gilsonmakanounou@yahoo.com>
2025-01-30 20:26:10 +0100

    feat: Ajout du modèle User avec mongoose

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const mongoose = require ('mongoose');
 
 const app = express();
 
 app.use(express.json());
+require('dotenv').config();
 
 
-app.use((req, res) => {
-    res.json({ message: 'Ceci est un test !' }); 
- });
+mongoose.connect(process.env.MONGODB,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
 
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 
 module.exports = app;
